999f606ab2fa50ae4152350092fe1b9872a857ab
Christopher Cartwright <cartwright.christopher@gmail.com>
2013-04-08 13:47:15 -0400

    Started work on custom NUnit addins
    - Added ReSharper configuration
    - Added project for NUnit addins
    - UnitTests requires the new addin class library
    -

--- GenToolsAddin/Properties/AssemblyInfo.cs
+++ GenToolsAddin/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using NUnit.Framework;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GenToolsAddin")]
+[assembly: AssemblyDescription("Provides required addins for GenTools")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("GenToolsAddin")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using NUnit.Framework;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ca51e6de-0057-4aa8-9dc8-ee7842a703ca")]
+[assembly: Guid("9186c6fd-2f1b-4835-babf-d6e72976d05e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,7 +31,5 @@ using NUnit.Framework;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: RequiredAddin("GenToolsAddin")]
---
+++ GenToolsAddin/RequireDecoratorAddin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Core;
+using NUnit.Core.Extensibility;
+
+namespace GenToolsAddin
+{
+	[NUnitAddin]
+	public class RequireDecoratorAddin : Attribute, IAddin, ITestDecorator
+	{
+		public bool Install(IExtensionHost host)
+		{
+			IExtensionPoint decorators = host.GetExtensionPoint("TestDecorators");
+			if (decorators == null)
+				return false;
+
+			decorators.Install(this);
+			return true;
+		}
+
+		public Test Decorate(Test test, MemberInfo member)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
